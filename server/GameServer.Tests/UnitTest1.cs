be5fdc2d1676487c0a3533aad5cc76e543fa0f4f
YuichiroSato <sato.yu1ro@gmail.com>
2022-09-08 23:21:41 +0900

    Update project to .net 6

---
+++ server/GameServer.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace GameServer.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
