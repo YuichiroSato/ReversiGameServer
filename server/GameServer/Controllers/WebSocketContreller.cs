4a293f9e94403bc6dab2250e9ae622a48479452f
YuichiroSato <sato.yu1ro@gmail.com>
2022-10-16 21:32:47 +0900

    Implement echo

--- server/GameServer/Controllers/WebSocketContreller.cs
+++ server/GameServer/Controllers/WebSocketContreller.cs
@@ -20,20 +20,29 @@ namespace GameServer.Controllers
         [HttpGet("connect")]
         public async Task Get()
         {
-            if (HttpContext.WebSockets.IsWebSocketRequest)
+            await Task.Run(async () =>
             {
-                using var webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
-                var buffer = new byte[] { (byte)'a', (byte)'b', (byte)'c' };
-                await webSocket.SendAsync(
-                    new ArraySegment<byte>(buffer, 0, 3),
-                    WebSocketMessageType.Text,
-                    true,
-                    CancellationToken.None);
-            }
-            else
-            {
-                HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
-            }
+                if (HttpContext.WebSockets.IsWebSocketRequest)
+                {
+                    var webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
+                    while(webSocket.State == WebSocketState.Connecting
+                        || webSocket.State == WebSocketState.Open)
+                    {
+                        var buffer = new byte[1024 * 4];
+                        var receiveResult = await webSocket.ReceiveAsync(
+                            new ArraySegment<byte>(buffer), CancellationToken.None);
+                        await webSocket.SendAsync(
+                            new ArraySegment<byte>(buffer, 0, receiveResult.Count),
+                            WebSocketMessageType.Text,
+                            true,
+                            CancellationToken.None);
+                    }
+                }
+                else
+                {
+                    HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+                }
+            });
         }
     }
 }
